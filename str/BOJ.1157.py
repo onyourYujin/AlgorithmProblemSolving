5706ff878db132858dbcdcac8d038a88abe6f885
onyourYujin <122829071+onyourYujin@users.noreply.github.com>
2023-02-19 17:24:25 +0100

    Create BOJ.1157.py

---
+++ str/BOJ.1157.py
@@ -0,0 +1,13 @@
+w = input().upper()
+words = list(set(w))
+cnt_list = []
+
+for x in words:
+    cnt = w.count(x)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list))>1:
+    print("?")
+else: 
+    max_index = cnt_list.index(max(cnt_list))
+    print(words[max_index])
