0df76ad85b392c184d04067d686989e678c6d5f9
onyourYujin <ys991120@gmail.com>
2023-02-22 22:29:40 +0100

    BOJ10813.py

---
+++ array/BOJ10813.py
@@ -0,0 +1,9 @@
+N,M=map(int,input().split())
+basket = [i for i in range(1,N+1)]
+
+for i in range(M):
+    a,b=map(int,input().split())
+    ball=basket[b-1]
+    basket[b-1]=basket[a-1]
+    basket[a-1]=ball
+print(basket)
