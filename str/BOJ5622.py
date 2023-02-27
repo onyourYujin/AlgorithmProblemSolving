9d2b607e9335e08318173242301869c50452151a
onyourYujin <122829071+onyourYujin@users.noreply.github.com>
2023-02-28 00:54:14 +0100

    Create BOJ5622.py

---
+++ str/BOJ5622.py
@@ -0,0 +1,9 @@
+word=input()
+lst=['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+time=0
+
+for i in range(len(word)):
+    for j in lst:
+        if word[i] in j:
+            time+=lst.index(j)+3
+print(time)
