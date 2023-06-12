f1b1245fb63dd0a2298df598e598cc397d7cb569
onyourYujin <ys991120@gmail.com>
2023-06-12 14:40:06 +0200

    BOJ1978(me).py

--- number_theory/BOJ1978.py
+++ number_theory/BOJ1978.py
@@ -1,11 +1,11 @@
-n = int(input())
-num_list = list(map(int,input().split()))
+N = int(input())
+N_list = list(map(int,input().split()))
 cnt = 0
 
-for x in num_list:
+for x in N_list:
     for y in range(2,x+1):
-        if x % y == 0:
-            if x == y:
+        if x%y == 0:
+            if y==1 or y==x:
                 cnt+=1
             break
 print(cnt)
