6018cc9e70589ebec2495ecbdd014894d3aff706
onyourYujin <ys991120@gmail.com>
2023-06-17 12:32:58 +0200

    BOJ5073.py

---
+++ Geometry/BOJ5073.py
@@ -0,0 +1,17 @@
+while True:
+    triangle = list(map(int,input().split()))
+    triangle.sort() 
+    a=triangle[0]
+    b=triangle[1]
+    c=triangle[2]
+    if triangle[0] == 0:
+        break
+    elif a+b<=c:
+        print("Invalid")
+    else:
+        if a==b==c :
+            print("Equilateral")
+        elif (a==b and a!=c) or (a==c and b!=c) or (b==c and a!=b):
+            print("Isosceles")
+        elif a!=b!=c:
+            print("Scalene")
