90ba41d1fd7ca683a8bd667493be45bf68166202
onyourYujin <ys991120@gmail.com>
2023-07-05 17:13:29 +0200

    진법변환

---
+++ Math/BOJ2745.py
@@ -0,0 +1,10 @@
+N,B = input().split()
+base = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+res=0
+for i, x in enumerate(N):
+    for j, y in enumerate(base):
+        if x == y:
+            cnt=j
+            break
+    res+=cnt*(int(B) ** (len(N)-i-1))
+print(res)
