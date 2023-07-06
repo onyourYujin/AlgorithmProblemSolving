c6c8a09032c4d57b0f06521844af37c3a4e84920
onyourYujin <ys991120@gmail.com>
2023-07-06 09:07:25 +0200

    소수

--- number_theory/BOJ2581.py
+++ number_theory/BOJ2581.py
@@ -2,13 +2,13 @@ m=int(input())
 n=int(input())
 list=[]
 for i in range(m,n+1):
-    e=0
+    cnt=0
     if i>1:
         for j in range(2,i):
             if i%j==0:
-                e+=1
+                cnt+=1
                 break
-        if e==0:
+        if cnt==0:
             li.append(i)
 
 if len(li)<1:
