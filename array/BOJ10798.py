8bcb702fbd3336d34a326434a22b65ab0dca9a59
onyourYujin <ys991120@gmail.com>
2023-07-13 23:05:06 +0200

    변수명 변경

--- array/BOJ10798.py
+++ array/BOJ10798.py
@@ -1,5 +1,5 @@
-word_list = [input() for i in range(5)]
+word = [input() for i in range(5)]
 for column in range(15):
     for row in range(5):
-        if column < len(word_list[row]):
-            print(word_list[row][column], end ='')
+        if column < len(word[row]):
+            print(word[row][column], end ='')
