ef15a7d29a89c663fe84d57d013cb4b8b7159d96
onyourYujin <ys991120@gmail.com>
2023-07-18 23:16:05 +0200

    변수명 변경

--- Math/BOJ2745.py
+++ Math/BOJ2745.py
@@ -1,10 +1,10 @@
 N,B = input().split()
 base = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-res=0
+result=0
 for i, x in enumerate(N):
     for j, y in enumerate(base):
         if x == y:
             cnt=j
             break
     res+=cnt*(int(B) ** (len(N)-i-1))
-print(res)
+print(result)
