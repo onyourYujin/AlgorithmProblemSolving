9c031b3ce450fb6a7ef19b03a8109c863bde2729
onyourYujin <ys991120@gmail.com>
2023-07-22 23:56:10 +0200

    변수명 변경

--- array/BOJ10798.py
+++ array/BOJ10798.py
@@ -2,4 +2,4 @@ word = [input() for i in range(5)]
 for column in range(15):
     for row in range(5):
         if column < len(word[row]):
-            print(word[row][column], end ='')
+            print(word[row][column], end =' ')
