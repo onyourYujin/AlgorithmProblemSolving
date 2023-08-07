5ae8ca4e52a0f91e872d00c5473e271b6d8bd823
onyourYujin <ys991120@gmail.com>
2023-08-07 23:46:16 +0200

    불필요한 코드 삭제

--- Control_Statement/BOJ1316.py
+++ Control_Statement/BOJ1316.py
@@ -1,12 +1,10 @@
 N = int(input())
-cnt = N
 for x in range(N):
     word = input()
-    new = set(word)
     for y in range(len(word)-1):
         if word[y] == word[y+1]:
             pass
         elif word[y] in word[y+1:]:
-            cnt-=1
+            N-=1
             break
-print(cnt)
+print(N)
