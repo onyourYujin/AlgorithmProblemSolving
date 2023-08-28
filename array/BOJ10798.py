dc908f2bbd5ce1f3e5b46112581ebb9479821a93
onyourYujin <ys991120@gmail.com>
2023-08-28 23:57:28 +0200

    end 수정

--- array/BOJ10798.py
+++ array/BOJ10798.py
@@ -2,4 +2,4 @@ words = [input() for i in range(5)]
 for column in range(15):
     for row in range(5):
         if column < len(words[row]):
-            print(words[row][column], end =' ')
+            print(words[row][column], end ='')
