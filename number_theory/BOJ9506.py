b6fbb9befa18fd992dc1bae9f3258f1e90a65372
onyourYujin <ys991120@gmail.com>
2023-08-29 23:47:33 +0200

    변수명 변경

--- number_theory/BOJ9506.py
+++ number_theory/BOJ9506.py
@@ -2,10 +2,10 @@ while True:
     n = int(input())
     if n == -1:
         break
-    lst = []
+    list = []
     for x in range(1,n):
         if n % x == 0:
-            lst.append(x)
+            list.append(x)
     if sum(lst) == n:
         print(n," = "," + ".join(str(i) for i in lst),sep="")
     else:
