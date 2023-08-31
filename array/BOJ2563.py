7966b181b00f5c896b594a42d4c71568a03081ac
onyourYujin <ys991120@gmail.com>
2023-08-31 23:44:09 +0200

    변수명 정리

--- array/BOJ2563.py
+++ array/BOJ2563.py
@@ -1,13 +1,13 @@
 N = int(input())
-paper = [[0 for _ in range(101)] for _ in range(101)]
+papers = [[0 for _ in range(101)] for _ in range(101)]
 
 for _ in range(N):
     x, y = map(int, input().split())
     for i in range(x, x+10):
         for j in range(y, y+10):
-            paper[i][j] = 1
+            papers[i][j] = 1
 
 answer = 0
-for row in paper:
-    answer += row.count(1)
+for paper in papers:
+    answer += paper.count(1)
 print(answer)
