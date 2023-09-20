f82cdfeeb615e969470409e502d77ce2989dbd11
onyourYujin <ys991120@gmail.com>
2023-09-20 10:54:05 +0200

    problem solving 230920

---
+++ brute_force/prog2_expression of num.py
@@ -0,0 +1,12 @@
+def solution(n):
+    cnt = 0
+    for i in range(1,n+1):
+        total = 0
+        for j in range(i,n+1):
+            total += j
+            if total == n:
+                cnt+=1
+                continue
+            elif total > n:
+                break
+    return cnt
