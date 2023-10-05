782098f9bd33d4bae8d1e581b7dc75193a2cab7e
onyourYujin <ys991120@gmail.com>
2023-10-05 23:45:19 +0200

    problem solving 231005

---
+++ Greedy/prog2_discount event.py
@@ -0,0 +1,13 @@
+def solution(want, number, discount):
+    cnt = 0
+    new_want = []
+    for i in range(len(want)):
+        for _ in range(number[i]):
+            new_want.append(want[i])
+        new_want.sort()
+    for j in range(len(discount)-9):
+        discount_10 = discount[j:j+10]
+        discount_10.sort()
+        if new_want == discount_10:
+            cnt += 1
+    return cnt
