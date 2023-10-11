3469d97c88fd6271a8a967da91df4984f0c792e0
onyourYujin <ys991120@gmail.com>
2023-10-11 16:06:15 +0200

    Add failure case

--- sorting and searching/prog2_H Index.py
+++ sorting and searching/prog2_H Index.py
@@ -1,3 +1,19 @@
+# ==============================================================================
+# 테스트 케이스 16에서 실패
+# h번 이상 인용되어야 하기 때문에 h의 최댓값은 citations의 개수 
+def solution(citations):
+    n = len(citations)
+    while n > 0:
+        cnt = 0
+        for j in citations:
+            if n <= j:
+                cnt+=1
+        if cnt == n:
+            return cnt
+        else:
+            n -= 1
+
+# ==============================================================================
 def solution(citations):
     citations.sort(reverse=True)
     n = len(citations)
