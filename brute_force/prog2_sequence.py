a8d6a954b8b5948bcac3a27907dd53875f30c7f9
onyourYujin <ys991120@gmail.com>
2023-10-17 12:10:57 +0200

    problem solving 231017

--- brute_force/prog2_sequence.py
+++ brute_force/prog2_sequence.py
@@ -2,7 +2,7 @@ def solution(elements):
     result = set()
     cycle = elements * 2
     leng = len(elements)
-    for i in range(leng):
-        for j in range(leng):
+    for i in range(leng):  # 몇 개의 연속된 수열을 합칠 지 결정
+        for j in range(leng):  # 부분 수열의 시작 위치
             result.add(sum(cycle[j:j+i+1]))
     return len(result)
