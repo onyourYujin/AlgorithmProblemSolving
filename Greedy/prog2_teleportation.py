82ea7b209621217e96397b56a4417607c2693b3c
onyourYujin <ys991120@gmail.com>
2023-10-19 23:50:25 +0200

    PEP8 change

--- Greedy/prog2_teleportation.py
+++ Greedy/prog2_teleportation.py
@@ -1,8 +1,8 @@
 def solution(n):
     battery = 0
-    while n>0:
-        if n%2 == 0:
-            n//=2
+    while n > 0:
+        if n % 2 == 0:
+            n // =2
         else:
             battery += 1
             n -= 1
