224094d0434c131ba7d75b44a16b9947ed3533f1
onyourYujin <ys991120@gmail.com>
2023-10-20 14:48:26 +0200

    problem solving 231020

--- brute_force/prog2_sequence.py
+++ brute_force/prog2_sequence.py
@@ -5,4 +5,5 @@ def solution(elements):
     for i in range(leng):  # 몇 개의 연속된 수열을 합칠 지 결정
         for j in range(leng):  # 부분 수열의 시작 위치
             result.add(sum(cycle[j:j+i+1]))
-    return len(result)
+    answer = len(result)
+    return answer
