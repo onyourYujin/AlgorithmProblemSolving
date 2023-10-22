3704fd9ecf73c0491125de492dc2bca767b5f3da
onyourYujin <ys991120@gmail.com>
2023-10-22 23:58:14 +0200

    problem solving 231022

--- Greedy/prog2_tangerine.py
+++ Greedy/prog2_tangerine.py
@@ -3,7 +3,7 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     count = sorted(Counter(tangerine).items(),reverse=True, key=lambda x:x[1])
-    # x[1] -> 튜플의 두번째 값을 기준으로 sort 한다는 뜻
+    # lambda -> 튜플의 두번째 값(x[1])을 기준으로 sort 한다는 뜻
 
     for key, value in count:
         if k <= 0 :
