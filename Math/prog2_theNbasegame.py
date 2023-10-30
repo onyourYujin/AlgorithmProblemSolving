fed94924d65c59669639c540f47f11b5bff8b060
onyourYujin <ys991120@gmail.com>
2023-10-30 23:07:30 +0100

    problem solving 231030

---
+++ Math/prog2_theNbasegame.py
@@ -0,0 +1,22 @@
+def numeral(n, b):
+    base = "0123456789ABCDEF"
+    s = ''
+    if n == 0:
+        return '0'
+    while n > 0:
+        s += base[n % b]
+        n //= b
+    return s[::-1]
+
+def solution(n, t, m, p):
+    answer = ''  # len(answer) == t
+    change = ''
+    i = 0
+    while True:
+        change += numeral(i, n)
+        i += 1
+        if len(change) > m * t:
+            break
+    for j in range(t):
+        answer += change[j * m + p - 1]
+    return answer
