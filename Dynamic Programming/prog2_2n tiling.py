75d500d2c460fed4def829da1e84e45ff2ba4146
onyourYujin <ys991120@gmail.com>
2023-11-17 16:39:41 +0100

    explain overflow

--- Dynamic Programming/prog2_2n tiling.py
+++ Dynamic Programming/prog2_2n tiling.py
@@ -1,5 +1,5 @@
 def solution(n):
     arr = [1,2]
     for i in range(2,n):
-        arr.append((arr[i-2]+arr[i-1]) % 1000000007)  # 시간초과 방지
+        arr.append((arr[i-2]+arr[i-1]) % 1000000007)  # 시간초과 방지 :계산 결과를 작은 값으로 유지하여 정확도를 향상
     return arr[-1]
