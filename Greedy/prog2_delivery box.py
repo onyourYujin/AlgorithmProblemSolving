455cb765808955ad351ade66bd9bd908170b14f3
onyourYujin <ys991120@gmail.com>
2023-11-22 11:27:07 +0100

    problem solving 231122

---
+++ Greedy/prog2_delivery box.py
@@ -0,0 +1,11 @@
+def solution(order):
+    sub = []
+    i = 1
+    cnt = 0
+    while len(order)+1 > i: # 트럭에 상자를 실을 수 있는 경우
+        sub.append(i)
+        while sub and order[cnt] == sub[-1]:  # 보조 컨테이너에 실어야 하는 경우
+            sub.pop()
+            cnt += 1
+        i += 1
+    return cnts
