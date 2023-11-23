7b7c92bf25a5d19bbbe813e7b3abb7f36f7f6f47
onyourYujin <ys991120@gmail.com>
2023-11-23 13:38:47 +0100

    problem solving 231123

---
+++ brute_force/prog2_find a prime num.py
@@ -0,0 +1,35 @@
+# 정확도 낮은 코드
+def is_prime(num):
+    num = int(num)
+    if num<2:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+from itertools import permutations
+def solution(numbers):  # str type
+    answer = []
+    nums = numbers.split()
+    prime = list(permutations(nums,2))
+    for i in prime:
+        nums.append("".join(i))
+    for i in nums:
+        if is_prime(i):
+            answer += 1
+    return answer
+
+# =================================================================================================================
+from itertools import permutations
+def solution(numbers):
+    answer = []
+    nums = [n for n in numbers]  # nums = numers.split()
+    per = []
+    for i in range(1,len(numbers)+1):
+        per+=list(permutations(nums,i))
+    new_nums = [int(("").join(p)) for p in per]
+    for i in new_nums:
+        if is_prime(i):
+            answer.append(i)
+    return len(set(answer))
