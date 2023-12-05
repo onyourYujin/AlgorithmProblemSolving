191286d7958de5a7b9cfe4fb05061d5773a4c108
onyourYujin <ys991120@gmail.com>
2023-12-05 15:41:39 +0100

    problem solving

--- Implementation/prog2_124 나라의 숫자.py
+++ Implementation/prog2_124 나라의 숫자.py
@@ -1,22 +1,24 @@
-# def solution(n):
-#     answer = ''
-#     while n > 0:
-#         if n % 3 == 0:
-#             n = n // 3 - 1
-#             answer += '4'
-#         else:
-#             answer += str(r)
-#         n = n // 3
-
-#     return answer[::-1]
-
+# 실패한 코드
 def solution(n):
     answer = ''
     while n > 0:
-        if n % 3:
-            answer += str(n % 3)
-            n //= 3
+        if n % 3 == 0:
+            n = n // 3 - 1
+            answer += '4'
         else:
-            answer += "4"
+            answer += str(r)
+        n = n // 3
+
+    return answer[::-1]
+
+# ========================================================================================
+def solution(n):
+    answer = ''
+    while n > 0:
+        if n % 3 == 0:
+            answer += '4'  # 마지막에도 몫은 추가하지 않고 나머지만 추가
             n = n//3 - 1
+        else:
+            answer += str(n%3)
+            n //= 3
     return answer[::-1]
