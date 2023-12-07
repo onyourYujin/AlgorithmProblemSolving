9aaac2eca46d11826f894780898800b1a46c55b9
onyourYujin <ys991120@gmail.com>
2023-12-07 14:14:06 +0100

    PEP 8

--- Greedy/prog2_두 큐 같게 만들기.py
+++ Greedy/prog2_두 큐 같게 만들기.py
@@ -3,10 +3,9 @@ from collections import deque
 
 def solution(queue1, queue2):
     answer = 0
-    q1,q2 = deque(queue1), deque(queue2)
+    q1, q2 = deque(queue1), deque(queue2)
     s1, s2 = sum(queue1), sum(queue2)
     answer, r = 0, len(q1) * 3
-    
     if (s1 + s2) % 2 != 0:
         return -1
     while True:
@@ -24,8 +23,6 @@ def solution(queue1, queue2):
             answer += 1
         else:
             break
-            
         if answer == r:
             return -1
-            
     return answer
