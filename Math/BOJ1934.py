f03a0754bd158a0bf2fcc7141d29e61624995857
onyourYujin <ys991120@gmail.com>
2023-12-19 23:36:06 +0100

    other method

--- Math/BOJ1934.py
+++ Math/BOJ1934.py
@@ -5,3 +5,11 @@ for _ in range(n):
     A,B = map(int,input().split())
     result = (A*B) // math.gcd(A,B)
     print(result)
+    
+# =========================================================================================
+import math
+
+n=int(input())
+for _ in range(n):
+    A,B = map(int,input().split())
+    print(math.lcm(A,B))
