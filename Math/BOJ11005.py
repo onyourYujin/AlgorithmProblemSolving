adbd7c97e54577e4fe9614400bb542e30925990f
onyourYujin <ys991120@gmail.com>
2023-12-24 12:09:22 +0100

    problem solving

--- Math/BOJ11005.py
+++ Math/BOJ11005.py
@@ -1,17 +1,17 @@
-N,B = map(int,input().split())
+N, B = map(int, input().split())
 base = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-li=[]
-new_li=[]
+li = []
+new_li = []
 
 while True:
-    li.append(N%B)
-    N=N//B
+    li.append(N % B)
+    N = N//B
 
-    if N == 0: 
+    if N == 0:
         break
 for i in li:
     for j in base:
         if j==base[i]:
             new_li.append(j)
 for x in new_li:
-    print(x,end="")
+    print(x, end="")
