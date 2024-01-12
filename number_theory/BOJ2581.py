ebac8114327be770e83594ef7257fafd1e37d982
onyourYujin <ys991120@gmail.com>
2024-01-12 15:57:10 +0100

    240112

--- number_theory/BOJ2581.py
+++ number_theory/BOJ2581.py
@@ -1,17 +1,18 @@
-m=int(input())
-n=int(input())
-list=[]
-for i in range(m,n+1):
-    cnt=0
-    if i>1:
-        for j in range(2,i):
-            if i%j==0:
-                cnt+=1
+m = int(input())
+n = int(input())
+list = []
+
+for i in range(m, n+1):
+    cnt = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j==0:
+                cnt += 1
                 break
-        if cnt==0:
+        if cnt == 0:
             li.append(i)
 
-if len(li)<1:
+if len(li) < 1:
     print(-1)
 else:
     print(sum(li))
